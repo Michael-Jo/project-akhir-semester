bc1fddbe300d532767c86f174d55b29b9929f328
Michael-Jo <michjo2008@gmail.com>
2023-11-17 18:24:52 +0700

    Forgot the ;

--- PatternRecognition.js
+++ PatternRecognition.js
@@ -2,4 +2,4 @@ document.addEventListener("DOMContentLoaded", () => {
     let myTimeOut = setTimeout(() => {
         document.querySelector('body').style.backgroundImage = "radial-gradient(white, black)";
     }, 1000);
-})
+});
